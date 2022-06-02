5535260673f354fd827d1e1b35e131d55427162f
Paladian78 <singhyogendar78@gmail.com>
2022-06-02 11:39:45 +0530

    resolved errors

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 
 
-# module "virtual_machine" {
-#     source = "./modules/virtual_machines"
-#     # create_reource_group = false
-#     prefix="myfrst"
-#     location= azurerm_resource_group.main.location
-#     resource_group_name= azurerm_resource_group.main.name
-# }
-
-module "app_service" {
-    source = "./modules/app_service"
+module "virtual_machine" {
+    source = "./modules/virtual_machines"
     # create_reource_group = false
     prefix="myfrst"
-    location= "eastasia"
-    resource_group_name= "myfrst-resources"
+    location= azurerm_resource_group.main.location
+    resource_group_name= azurerm_resource_group.main.name
 }
+
+# module "app_service" {
+#     source = "./modules/app_service"
+#     # create_reource_group = false
+#     prefix="myfrst"
+#     location= "eastasia"
+#     resource_group_name= "myfrst-resources"
+# }
   
---
+++ virtual_machines/variables.tf
@@ -0,0 +1,12 @@
+variable "prefix" {
+  description = "myfrst"
+}
+
+variable "location" {
+  description = "eastasia"
+}
+
+variable "resource_group_name" {
+  description = "resource group name where you want to deploy your virtual machine"
+  default     = "myfrst-resources"
+}
