58e6fcb2ac96c9aac2db4ba4b48a495b79f74e25
Paladian78 <singhyogendar78@gmail.com>
2022-06-02 21:45:26 +0530

    resolved errors

--- modules/logic_app/main.tf
+++ modules/logic_app/main.tf
@@ -3,7 +3,7 @@
 resource "azurerm_app_service_plan" "main" {
   name                = "azure-functions-test-service-plan"
   location            = var.location
-  resource_group_name = var.resorce_group_name
+  resource_group_name = var.resource_group_name
 
   sku {
     tier = "WorkflowStandard"
@@ -14,7 +14,7 @@ resource "azurerm_app_service_plan" "main" {
 resource "azurerm_logic_app_standard" "main" {
   name                       = "test-azure-functions"
   location                   = var.location
-  resource_group_name        = var.resorce_group_name
+  resource_group_name        = var.resource_group_name
   app_service_plan_id        = azurerm_app_service_plan.main.id
   storage_account_name       = var.storage_account_name
   storage_account_access_key = var.storage_account_access_key
--- modules/storage_account/main.tf
+++ modules/storage_account/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "main" {
-  name = var.resorce_group_name
+  name = var.resource_group_name
   location = var.location
 }
 output "rg_name" {
