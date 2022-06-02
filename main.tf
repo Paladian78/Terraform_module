f005c9575b1d7392226a57adcd3afed21da04cf8
Paladian78 <singhyogendar78@gmail.com>
2022-06-02 11:21:25 +0530

    resolved errors

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ resource "azurerm_resource_group" "main" {
   location = "eastasia"
 }
 
-module "virtual_machine" {
-    source = "./modules/virtual_machines"
-    # create_reource_group = false
-    prefix="myfrst"
-    location= azurerm_resource_group.main.location
-    resource_group_name= azurerm_resource_group.main.name
-}
+# module "virtual_machine" {
+#     source = "./modules/virtual_machines"
+#     # create_reource_group = false
+#     prefix="myfrst"
+#     location= azurerm_resource_group.main.location
+#     resource_group_name= azurerm_resource_group.main.name
+# }
 
 module "app_service" {
     source = "./modules/app_service"
