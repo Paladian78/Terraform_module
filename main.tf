db8ede0d3ffb68d2d963b80a2ee5c118b1073ff4
Paladian78 <singhyogendar78@gmail.com>
2022-06-02 11:18:34 +0530

    resolved errors

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "main" {
 
 module "virtual_machine" {
     source = "./modules/virtual_machines"
-    create_reource_group = false
+    # create_reource_group = false
     prefix="myfrst"
     location= azurerm_resource_group.main.location
     resource_group_name= azurerm_resource_group.main.name
