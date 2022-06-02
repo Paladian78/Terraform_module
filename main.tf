9c83cc05df3ed59a2dc4a8e75dac91b82f79e110
Paladian78 <singhyogendar78@gmail.com>
2022-06-02 21:58:55 +0530

    resolved errors

--- main.tf
+++ main.tf
@@ -16,3 +16,11 @@ module "logic_app" {
     storage_account_name = module.storage_account.strg_name
     storage_account_access_key = module.storage_account.strg_key
 }
+
+module "key_vault" {
+  source = "./modules/key_vault"
+  resource_group_name = module.storage_account.rg_name
+    location = var.location
+    key_vault_name = "test-key-vault"
+}
+  
