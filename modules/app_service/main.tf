6e37a7610b98c12ee08a5de9fab610baa10cef70
Paladian78 <singhyogendar78@gmail.com>
2022-06-02 11:32:05 +0530

    resolved errors

--- modules/app_service/main.tf
+++ modules/app_service/main.tf
@@ -1,9 +1,12 @@
-
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
   
 resource "azurerm_service_plan" "main" {
   name                = "${var.prefix}-aappsp"
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.main.name
   os_type             = "Windows"
   sku_name            = "B1"
 }
@@ -11,7 +14,7 @@ resource "azurerm_service_plan" "main" {
 resource "azurerm_windows_web_app" "main" {
   name                = "${var.prefix}-basic-xample"
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.main.name
   service_plan_id     = azurerm_service_plan.main.id
 
   site_config {}
