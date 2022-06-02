e5d9c2a8ffccf92389797d368c0bdf3e93808d2e
Paladian78 <singhyogendar78@gmail.com>
2022-06-02 11:19:59 +0530

    resolved errors

--- modules/app_service/main.tf
+++ modules/app_service/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_windows_web_app" "main" {
   name                = "${var.prefix}-basic-xample"
   location            = var.location
   resource_group_name = var.resource_group_name
-  service_plan_id     = azurerm_service_plan.example.id
+  service_plan_id     = azurerm_service_plan.main.id
 
   site_config {}
 }
