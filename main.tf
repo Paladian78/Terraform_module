574015294c71acfd5a554b2701fb272b98ec3696
Paladian78 <singhyogendar78@gmail.com>
2022-06-02 11:43:42 +0530

    resolved errors

--- main.tf
+++ main.tf
@@ -4,6 +4,8 @@ module "virtual_machine" {
     source = "./modules/virtual_machines"
     # create_reource_group = false
     prefix="myfrst"
+    location="eastasia"
+    resource_group_name="myfrst-resources"
 }
 
 # module "app_service" {
