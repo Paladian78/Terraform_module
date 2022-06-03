48d047f2a95aa1e2069c30039376aa2a38829630
Paladian78 <singhyogendar78@gmail.com>
2022-06-04 00:28:44 +0530

    added modules

--- modules/logic_app/main.tf
+++ modules/logic_app/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_logic_app_standard" "main" {
   name                       = "mylogicaappp"
   location                   = var.location
   resource_group_name        = var.resource_group_name
-  app_service_plan_id        = var.app_service_plan_id
+  app_service_plan_id        = azurerm_app_service_plan.main.id
   storage_account_name       = var.storage_account_name
   storage_account_access_key = var.storage_account_access_key
 }
