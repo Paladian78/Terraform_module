e2fed1b6baba18445c97c216c71d12456826db32
Paladian78 <singhyogendar78@gmail.com>
2022-06-03 19:26:31 +0530

    resolved error

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ module "app_service" {
   source = "./modules/app_service"
   resource_group_name = var.resource_group_name
     location = var.location
-
+    function_app_name = "azure-functions_app_service"
     app_service_plan_id = module.appservice_plan.app_service_plan_id
 }
 
